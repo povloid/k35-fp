5998620edbb09b67ce16a176f43b4bbf14e0fa36
Pavel Kopychenko <pkopychenko@inko-med.ru>
2025-06-26 12:16:00 +0300

    Deleted old curry functions.

--- dist/fp.js
+++ dist/fp.js
@@ -7,56 +7,6 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
-export var partial1 = function (fn, a1) { return function (a2) { return fn(a1, a2); }; };
-export var partial2 = function (fn, a1, a2) { return function (a3) { return fn(a1, a2, a3); }; };
-export var partial3 = function (fn, a1, a2, a3) { return function (a4) { return fn(a1, a2, a3, a4); }; };
-export var partial4 = function (fn, a1, a2, a3, a4) { return function (a5) { return fn(a1, a2, a3, a4, a5); }; };
-export var partial5 = function (fn, a1, a2, a3, a4, a5) { return function (a6) { return fn(a1, a2, a3, a4, a5, a6); }; };
-export var partial6 = function (fn, a1, a2, a3, a4, a5, a6) { return function (a7) { return fn(a1, a2, a3, a4, a5, a6, a7); }; };
-export var partial7 = function (fn, a1, a2, a3, a4, a5, a6, a7) { return function (a8) { return fn(a1, a2, a3, a4, a5, a6, a7, a8); }; };
-export var partial1Right = function (fn, a2) { return function (a1) { return fn(a1, a2); }; };
-export var partial2Right = function (fn, a2, a3) { return function (a1) { return fn(a1, a2, a3); }; };
-export var partial3Right = function (fn, a2, a3, a4) { return function (a1) { return fn(a1, a2, a3, a4); }; };
-export var partial4Right = function (fn, a2, a3, a4, a5) { return function (a1) { return fn(a1, a2, a3, a4, a5); }; };
-export var partial5Right = function (fn, a2, a3, a4, a5, a6) { return function (a1) { return fn(a1, a2, a3, a4, a5, a6); }; };
-export var partial6Right = function (fn, a2, a3, a4, a5, a6, a7) { return function (a1) { return fn(a1, a2, a3, a4, a5, a6, a7); }; };
-export var partial7Right = function (fn, a2, a3, a4, a5, a6, a7, a8) { return function (a1) { return fn(a1, a2, a3, a4, a5, a6, a7, a8); }; };
-export var curry2 = function (fn) {
-    return function (a1) { return function (a2) { return fn(a1, a2); }; };
-};
-export var curry3 = function (fn) {
-    return function (a1) { return function (a2) { return function (a3) { return fn(a1, a2, a3); }; }; };
-};
-export var curry4 = function (fn) {
-    return function (a1) { return function (a2) { return function (a3) { return function (a4) { return fn(a1, a2, a3, a4); }; }; }; };
-};
-export var curry5 = function (fn) {
-    return function (a1) { return function (a2) { return function (a3) { return function (a4) { return function (a5) { return fn(a1, a2, a3, a4, a5); }; }; }; }; };
-};
-export var curry6 = function (fn) {
-    return function (a1) { return function (a2) { return function (a3) { return function (a4) { return function (a5) { return function (a6) { return fn(a1, a2, a3, a4, a5, a6); }; }; }; }; }; };
-};
-export var curry7 = function (fn) {
-    return function (a1) { return function (a2) { return function (a3) { return function (a4) { return function (a5) { return function (a6) { return function (a7) { return fn(a1, a2, a3, a4, a5, a6, a7); }; }; }; }; }; }; };
-};
-export var curry2Right = function (fn) {
-    return function (a2) { return function (a1) { return fn(a1, a2); }; };
-};
-export var curry3Right = function (fn) {
-    return function (a3) { return function (a2) { return function (a1) { return fn(a1, a2, a3); }; }; };
-};
-export var curry4Right = function (fn) {
-    return function (a4) { return function (a3) { return function (a2) { return function (a1) { return fn(a1, a2, a3, a4); }; }; }; };
-};
-export var curry5Right = function (fn) {
-    return function (a5) { return function (a4) { return function (a3) { return function (a2) { return function (a1) { return fn(a1, a2, a3, a4, a5); }; }; }; }; };
-};
-export var curry6Right = function (fn) {
-    return function (a6) { return function (a5) { return function (a4) { return function (a3) { return function (a2) { return function (a1) { return fn(a1, a2, a3, a4, a5, a6); }; }; }; }; }; };
-};
-export var curry7Right = function (fn) {
-    return function (a7) { return function (a6) { return function (a5) { return function (a4) { return function (a3) { return function (a2) { return function (a1) { return fn(a1, a2, a3, a4, a5, a6, a7); }; }; }; }; }; }; };
-};
 export function compose() {
     var functions = [];
     for (var _i = 0; _i < arguments.length; _i++) {
