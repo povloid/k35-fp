b07fa1fe802d76ed474ea1c64e29ad4b10e3aeac
Pavel Kopychenko <pkopychenko@inko-med.ru>
2025-06-26 12:16:00 +0300

    Deleted old curry functions.

--- src/fp.ts
+++ src/fp.ts
@@ -1,71 +1,4 @@
 
-export const partial1 = <A1, A2, R>(fn: (a1: A1, a2: A2) => R, a1: A1) => (a2: A2) => fn(a1, a2)
-export const partial2 = <A1, A2, A3, R>(fn: (a1: A1, a2: A2, a3: A3) => R, a1: A1, a2: A2) => (a3: A3) => fn(a1, a2, a3)
-export const partial3 = <A1, A2, A3, A4, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4) => R, a1: A1, a2: A2, a3: A3) => (a4: A4) => fn(a1, a2, a3, a4)
-export const partial4 = <A1, A2, A3, A4, A5, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R, a1: A1, a2: A2, a3: A3, a4: A4) => (a5: A5) => fn(a1, a2, a3, a4, a5)
-export const partial5 = <A1, A2, A3, A4, A5, A6, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => R, a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => (a6: A6) => fn(a1, a2, a3, a4, a5, a6)
-export const partial6 = <A1, A2, A3, A4, A5, A6, A7, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => R, a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => (a7: A7) => fn(a1, a2, a3, a4, a5, a6, a7)
-export const partial7 = <A1, A2, A3, A4, A5, A6, A7, A8, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7, a8: A8) => R, a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => (a8: A8) => fn(a1, a2, a3, a4, a5, a6, a7, a8)
-
-export const partial1Right = <A1, A2, R>(fn: (a1: A1, a2: A2) => R, a2: A2) => (a1: A1) => fn(a1, a2)
-export const partial2Right = <A1, A2, A3, R>(fn: (a1: A1, a2: A2, a3: A3) => R, a2: A2, a3: A3) => (a1: A1) => fn(a1, a2, a3)
-export const partial3Right = <A1, A2, A3, A4, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4) => R, a2: A2, a3: A3, a4: A4) => (a1: A1) => fn(a1, a2, a3, a4)
-export const partial4Right = <A1, A2, A3, A4, A5, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R, a2: A2, a3: A3, a4: A4, a5: A5) => (a1: A1) => fn(a1, a2, a3, a4, a5)
-export const partial5Right = <A1, A2, A3, A4, A5, A6, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => R, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => (a1: A1) => fn(a1, a2, a3, a4, a5, a6)
-export const partial6Right = <A1, A2, A3, A4, A5, A6, A7, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => R, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => (a1: A1) => fn(a1, a2, a3, a4, a5, a6, a7)
-export const partial7Right = <A1, A2, A3, A4, A5, A6, A7, A8, R>(fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7, a8: A8) => R, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7, a8: A8) => (a1: A1) => fn(a1, a2, a3, a4, a5, a6, a7, a8)
-
-
-export const curry2 = <A1, A2, R>
-    (fn: (a1: A1, a2: A2) => R) =>
-    (a1: A1) => (a2: A2) => fn(a1, a2)
-
-export const curry3 = <A1, A2, A3, R>
-    (fn: (a1: A1, a2: A2, a3: A3) => R) =>
-    (a1: A1) => (a2: A2) => (a3: A3) => fn(a1, a2, a3)
-
-export const curry4 = <A1, A2, A3, A4, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4) => R) =>
-    (a1: A1) => (a2: A2) => (a3: A3) => (a4: A4) => fn(a1, a2, a3, a4)
-
-export const curry5 = <A1, A2, A3, A4, A5, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R) =>
-    (a1: A1) => (a2: A2) => (a3: A3) => (a4: A4) => (a5: A5) => fn(a1, a2, a3, a4, a5)
-
-export const curry6 = <A1, A2, A3, A4, A5, A6, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => R) =>
-    (a1: A1) => (a2: A2) => (a3: A3) => (a4: A4) => (a5: A5) => (a6: A6) => fn(a1, a2, a3, a4, a5, a6)
-
-export const curry7 = <A1, A2, A3, A4, A5, A6, A7, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => R) =>
-    (a1: A1) => (a2: A2) => (a3: A3) => (a4: A4) => (a5: A5) => (a6: A6) => (a7: A7) => fn(a1, a2, a3, a4, a5, a6, a7)
-
-
-export const curry2Right = <A1, A2, R>
-    (fn: (a1: A1, a2: A2) => R) =>
-    (a2: A2) => (a1: A1) => fn(a1, a2)
-
-export const curry3Right = <A1, A2, A3, R>
-    (fn: (a1: A1, a2: A2, a3: A3) => R) =>
-    (a3: A3) => (a2: A2) => (a1: A1) => fn(a1, a2, a3)
-
-export const curry4Right = <A1, A2, A3, A4, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4) => R) =>
-    (a4: A4) => (a3: A3) => (a2: A2) => (a1: A1) => fn(a1, a2, a3, a4)
-
-export const curry5Right = <A1, A2, A3, A4, A5, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => R) =>
-    (a5: A5) => (a4: A4) => (a3: A3) => (a2: A2) => (a1: A1) => fn(a1, a2, a3, a4, a5)
-
-export const curry6Right = <A1, A2, A3, A4, A5, A6, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => R) =>
-    (a6: A6) => (a5: A5) => (a4: A4) => (a3: A3) => (a2: A2) => (a1: A1) => fn(a1, a2, a3, a4, a5, a6)
-
-export const curry7Right = <A1, A2, A3, A4, A5, A6, A7, R>
-    (fn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6, a7: A7) => R) =>
-    (a7: A7) => (a6: A6) => (a5: A5) => (a4: A4) => (a3: A3) => (a2: A2) => (a1: A1) => fn(a1, a2, a3, a4, a5, a6, a7)
-
-
 /**
  * Комзозиция функций
  * 
@@ -102,7 +35,7 @@ export function composel(...functions: Function[]) {
  *  (...rest) => (first) => f(first, ...rest)
  * @param fn функция
  */
-export function asUpdate<T1, R>(fn: (arg1: T1) => R): (arg1: T1) => R;
+export function asUpdate<T1, R>(fn: (arg1: T1) => R): (arg1: T1) => () => R;
 export function asUpdate<T1, T2, R>(fn: (arg1: T1, arg2: T2) => R): (arg2: T2) => (arg1: T1) => R;
 export function asUpdate<T1, T2, T3, R>(fn: (arg1: T1, arg2: T2, arg3: T3) => R): (arg2: T2, arg3: T3) => (arg1: T1) => R;
 export function asUpdate<T1, T2, T3, T4, R>(fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => R): (arg2: T2, arg3: T3, arg4: T4) => (arg1: T1) => R;
